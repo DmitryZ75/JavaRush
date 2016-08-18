c2784926ca98eb7fcce17b1719700ebb03e6de07
Dmitry Zhagrin <mail@dmitryz.com>
2016-08-18 10:59:17 +0300

    lesson08task0405
    done

--- test/level06/lesson08/task04/ConsoleReader.java
+++ test/level06/lesson08/task04/ConsoleReader.java
@@ -16,25 +16,28 @@ public class ConsoleReader
 {
     public static String readString() throws Exception
     {
-        //напишите тут ваш код
-
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
     }
 
     public static int readInt() throws Exception
     {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(reader.readLine());
 
     }
 
     public static double readDouble() throws Exception
     {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Double.parseDouble(reader.readLine());
 
     }
 
     public static boolean readBoolean() throws Exception
     {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Boolean.parseBoolean(reader.readLine());
 
     }
 }
